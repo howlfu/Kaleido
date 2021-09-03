d78037933b9212c99a609a9b180a221b15ccd982
howlfu <howlfu@gmail.com>
2021-09-03 14:32:31 +0800

    create with core data

---
+++ Kaleido/Kaleido/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kaleido
+//
+//  Created by Civet on 2021/9/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
