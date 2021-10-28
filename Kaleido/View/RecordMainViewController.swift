c5d0464067af2170e6f946b753f616f4c4c5663b
howlfu <howlfu@gmail.com>
2021-10-28 14:37:12 +0800

    add record view

---
+++ Kaleido/View/RecordMainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  recordMainViewController.swift
+//  Kaleido
+//
+//  Created by Civet on 2021/10/28.
+//
+
+import UIKit
+
+class RecordMainViewController: UIViewController{
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+//        self.navigationController?.interactivePopGestureRecognizer!.delegate = self;
+//        self.navigationController?.interactivePopGestureRecognizer!.delegate = nil;
+    }
+//    override func viewDidDisappear(_ animated: Bool) {
+//        self.navigationController?.interactivePopGestureRecognizer!.isEnabled = true;
+//    }
+//
+//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//            return true
+//        }
+}
