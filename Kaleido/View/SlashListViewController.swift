938142fa41784ba7259bad798838c7175f553bf2
howlfu <howlfu@gmail.com>
2021-11-01 14:39:42 +0800

    working on slash lis view

--- Kaleido/View/SlashListViewController.swift
+++ Kaleido/View/SlashListViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 class SlashListViewController: BaseViewController{
     @IBOutlet weak var listTableView: UITableView!
     @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var toNextViewBtn: UIButton!
+    var selectedCellArr: [SlashListType] = []
     let aCellHightOfViewRadio = 8
     override func initView() {
         super.initView()
         titleView.roundedBottRight(radius: 100)
+        toNextViewBtn.layer.cornerRadius = 20
+        listTableView.separatorStyle = .none
     }
 }
 
-extension SlashListViewController: UITableViewDataSource {
-
+extension SlashListViewController: UITableViewDataSource, UITableViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.listTableView.delegate = self
@@ -25,36 +29,51 @@ extension SlashListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableCellHight = self.view.frame.height / CGFloat(aCellHightOfViewRadio)
-        var cell: UITableViewCell? = nil
-        cell  = tableView.dequeueReusableCell(withIdentifier: "slashListCell", for: indexPath)
-        let statusBG : UIImageView = cell!.contentView.viewWithTag(3) as! UIImageView
-        let statusIcon : UIImageView = cell!.contentView.viewWithTag(1) as! UIImageView
-        let lblTitle : UILabel = cell!.contentView.viewWithTag(2) as! UILabel
-        statusIcon.frame.origin.y = (tableCellHight - statusIcon.frame.size.height) / 2
-        statusBG.frame.origin.y = (tableCellHight - statusBG.frame.size.height) / 2
-        lblTitle.frame.origin.y = (tableCellHight - lblTitle.frame.size.height) / 2
-        lblTitle.text = SlashListType.allCases[indexPath.row].rawValue
         
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "slashListCell", for: indexPath)
+        let statusIcon : UIImageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let lblTitle : UILabel = cell.contentView.viewWithTag(2) as! UILabel
+        let statusBG : UIImageView = cell.contentView.viewWithTag(3) as! UIImageView
+        let backgroundView : UIView = cell.contentView.viewWithTag(4)!
+        statusIcon.frame.origin.y = (backgroundView.frame.height - statusIcon.frame.size.height) / 2
+        statusBG.frame.origin.y = (backgroundView.frame.height - statusBG.frame.size.height) / 2
+        lblTitle.frame.origin.y = (backgroundView.frame.height - lblTitle.frame.size.height) / 2
+        lblTitle.text = SlashListType.allCases[indexPath.row].rawValue
+        cell.contentView.frame.size.width = (cell.contentView.frame.size.width * 2) / 3
+        cell.contentView.frame.size.height = (cell.contentView.frame.size.height * 2) / 3
+        backgroundView.layer.cornerRadius = 20
+        backgroundView.sendSubviewToBack(cell)
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return SlashListType.allCases.count
     }
-    
-
-    
-    
-}
-
-extension SlashListViewController: UITableViewDelegate {
+    //UITableViewDelegate
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-           return self.view.frame.height / CGFloat(aCellHightOfViewRadio)
+        return listTableView.frame.height / CGFloat(aCellHightOfViewRadio)
        }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//            let rowIndex:Int = indexPath.row
-//            self.selectBtn(rowIndex)
+        let selectedCell = tableView.cellForRow(at: indexPath)
+        let rowIndex:Int = indexPath.row
+        cellSelected(cell: selectedCell, index: rowIndex)
+    }
+    
+    
+    func cellSelected(cell: UITableViewCell?, index: Int) {
+        guard let selectedCell = cell else {
+            return
         }
+        let statusIcon : UIImageView = selectedCell.contentView.viewWithTag(1) as! UIImageView
+        let selectedListType = SlashListType.allCases[index]
+        if selectedCellArr.contains(selectedListType) {
+            selectedCellArr.remove(at: selectedCellArr.firstIndex(of: selectedListType)!)
+            statusIcon.image = nil
+        } else {
+            selectedCellArr.append(selectedListType)
+            statusIcon.image =  UIImage(named: "Checked")
+        }
+    }
 }
+
