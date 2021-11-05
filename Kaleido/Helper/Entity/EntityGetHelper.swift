40960b5c9672f1b03ed2dfce553e4c79fc8aeda0
howlfu <howlfu@gmail.com>
2021-11-05 15:59:29 +0800

    add get order

--- Kaleido/Helper/Entity/EntityGetHelper.swift
+++ Kaleido/Helper/Entity/EntityGetHelper.swift
@@ -10,24 +10,59 @@ import Foundation
 class EntityGetHelper {
     static let inst = EntityGetHelper()
     private let crudService = EntityCRUDService()
-    public func getCustomer(by name: String) -> [Customer]? {
+    
+    public func getCustomer(id: Int) -> [Customer]? {
+        let result: [Customer] = self.getCustomerByRule(with: "id=\(id)")
+        return result
+    }
+    
+    public func getCustomer(name: String) -> [Customer]? {
         let result: [Customer] = self.getCustomerByRule(with: "full_name='\(name)'")
         return result
     }
     
-//    public func getCustomer(by birthday: Date) -> [Customer]?{
-//        let result: [Customer] = self.getCustomerByRule(with: "birthday=\(birthday)")
-//        return result
-//    }
+    public func getCustomer(birthday: String) -> [Customer]?{
+        let result: [Customer] = self.getCustomerByRule(with: "birthday=\(birthday)")
+        return result
+    }
     
-    public func getCustomer(with phone: String) -> [Customer]?{
+    public func getCustomer(phone: String) -> [Customer]?{
         let result: [Customer] = self.getCustomerByRule(with: "phone_number=\(phone)")
         return result
     }
     
     private func getCustomerByRule<T>(with rule: String) -> [T] {
         let EntityName = EntityNameDefine.customer.rawValue
-        let customerEntity = crudService.readData(name: EntityName, with: rule)
+        return self.getDataBase(entity: EntityName, rule: rule)
+    }
+    
+    public func getAllCustomerEntitys() -> [Customer] {
+        let EntityName = EntityNameDefine.customer.rawValue
+        return getEntityAllDataBase(entity: EntityName)
+    }
+    
+    public func getOrders(id: Int) -> [Order]?{
+        let result: [Order] = self.getOrderByRule(with: "id=\(id)")
+        return result
+    }
+    
+    public func getOrders(uId: Int) -> [Order]?{
+        let result: [Order] = self.getOrderByRule(with: "user_id=\(uId)")
+        return result
+    }
+    
+    public func getAllOrders() {
+        let EntityName = EntityNameDefine.order.rawValue
+        return getEntityAllDataBase(entity: EntityName)
+    }
+    
+    private func getOrderByRule<T>(with rule: String) -> [T] {
+        let EntityName = EntityNameDefine.order.rawValue
+        return self.getDataBase(entity: EntityName, rule: rule)
+    }
+    
+    private func getDataBase<T>(entity: String, rule: String) -> [T] {
+        let customerEntity = crudService.readData(name: entity, with: rule)
         var retData: [T] = []
         for customerData in customerEntity {
             if let getCustomer = customerData as? T{
@@ -37,29 +72,14 @@ class EntityGetHelper {
         return retData
     }
     
-    private func getAllCustomerEntity() -> [Customer] {
-        let EntityName = EntityNameDefine.customer.rawValue
-        let customerEntity = crudService.readData(by: EntityName)
-        var retData: [Customer] = []
-        for customerData in customerEntity {
-            if let getCustomer = customerData as? Customer{
-                retData.append(getCustomer)
+    private func getEntityAllDataBase<T>(entity: String) -> [T] {
+        let entityAllData = crudService.readData(by: entity)
+        var retData: [T] = []
+        for singleData in entityAllData {
+            if let custSingleData = singleData as? T{
+                retData.append(custSingleData)
             }
         }
         return retData
     }
-    
-    public func getOrders(by id: String) {
-        
-    }
-    
-    public func getOrders(with custName: String) {
-        
-    }
-    
-    private func getAllOrders() {
-        
-    }
-    
-    
 }
