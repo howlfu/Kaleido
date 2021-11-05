d34a5eed6c06d924b473127493a816f3e4f182b1
howlfu <howlfu@gmail.com>
2021-11-05 15:17:40 +0800

    implement get Customer data

--- Kaleido/Helper/Entity/EntityGetHelper.swift
+++ Kaleido/Helper/Entity/EntityGetHelper.swift
@@ -9,21 +9,44 @@ import Foundation
 
 class EntityGetHelper {
     static let inst = EntityGetHelper()
-    
-    public func getCustomer(by name: String) {
-        
+    private let crudService = EntityCRUDService()   
+    public func getCustomer(by name: String) -> [Customer]? {
+        let result: [Customer] = self.getCustomerByRule(with: "full_name='\(name)'")
+        return result
     }
     
-    public func getCustomer(by bith: Date) {
-        
+//    public func getCustomer(by birthday: Date) -> [Customer]?{
+//        let result: [Customer] = self.getCustomerByRule(with: "birthday=\(birthday)")
+//        return result
+//    }
+    
+    public func getCustomer(with phone: String) -> [Customer]?{
+        let result: [Customer] = self.getCustomerByRule(with: "phone_number=\(phone)")
+        return result
     }
     
-    public func getCustomer(with phone: String) {
-        
+    private func getCustomerByRule<T>(with rule: String) -> [T] {
+        let EntityName = EntityNameDefine.customer.rawValue
+        let customerEntity = crudService.readData(name: EntityName, with: rule)
+        var retData: [T] = []
+        for customerData in customerEntity {
+            if let getCustomer = customerData as? T{
+                retData.append(getCustomer)
+            }
+        }
+        return retData
     }
     
-    private func getAllCustomerEntity() {
-        
+    private func getAllCustomerEntity() -> [Customer] {
+        let EntityName = EntityNameDefine.customer.rawValue
+        let customerEntity = crudService.readData(by: EntityName)
+        var retData: [Customer] = []
+        for customerData in customerEntity {
+            if let getCustomer = customerData as? Customer{
+                retData.append(getCustomer)
+            }
+        }
+        return retData
     }
     
     public func getOrders(by id: String) {
---
+++ Kaleido/Helper/Entity/EntityNameDefine.swift
@@ -0,0 +1,12 @@
+//
+//  EntityNameDefine.swift
+//  Kaleido
+//
+//  Created by Howlfu on 2021/11/5.
+//
+
+import Foundation
+enum EntityNameDefine : String{
+    case customer = "Customer"
+    case order = "Order"
+}
